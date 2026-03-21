fc02c0a74b7c9e885d8c595f00caed418f2ae4d3
Luciana Kyoko <lukyoko@protonmail.com>
2026-03-21 20:53:03 -0300

    feat(services/donationService): add fetchAllDonationGroupedByAnimal function

--- src/services/donationService.ts
+++ src/services/donationService.ts
@@ -1,6 +1,12 @@
 import api from "@/lib/api";
+import { DonationGroup } from "../types";
 
 export const fetchDonationSummary = async () => {
   const response = await api.get('/donation/summary');
+  return response.data;
+}
+
+export const fetchAllDonationGroupedByAnimal = async (): Promise<DonationGroup[]> => {
+  const response = await api.get<DonationGroup[]>('/donation/animals');
   return response.data;
 }
